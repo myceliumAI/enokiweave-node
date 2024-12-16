cc4fb7722ecffd58bf5e50e1a2435c9caf382fb7
Luc Grandin <104393342+GrandinLuc@users.noreply.github.com>
2024-12-16 12:05:12 +0100

    feat: added CLI to build your transactions

--- ReadMe.md
+++ ReadMe.md
@@ -33,22 +33,31 @@ git clone https://github.com/enokiweave/enokiweave
 git clone https://github.com/enokiweave/enokiweave
 ```
 
-# Send a transaction
+# Send a transaction (the node needs to be running)
 ```bash
 echo '{
     "jsonrpc": "2.0",
     "method": "submitTransaction",
     "params": [{
-        "from": "0x0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20",
-        "to":   "0x201f1e1d1c1b1a191817161514131211100f0e0d0c0b0a090807060504030201",
+        "from": "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20",
+        "to":   "201f1e1d1c1b1a191817161514131211100f0e0d0c0b0a090807060504030201",
         "amount": 100,
-        "public_key": "f7a4654958b940d85ef8c3c40afea448f75a236177adbfc2c27da1953bc68b70",
+        "public_key": "3b6a27bcceb6a42d62a3a8d02a6f0d73653215771de243a63ac048a18b59da29",
         "signature": {
-            "R": "9b3d055ee6a487d46729cb480a1404ad0aa1cc634d734c8ce84673a83b55b126",
-            "s": "28f2e14c9fb932301734a86f6e6c1a2d7bf9ebab0973a42e491679fe9d7ee80d"
+            "R": "d3b9bc2c6224e1b0d327f83f2fba25b66f58ea7c87c98a90b9f7f99f4e870be4",
+            "s": "0acefe7c263262675dc07f0f270795cf319bd0bb8734dda8d28f055bfa1aa70f"
         },
-        "timestamp": 1709159751000,
-        "id": "1d2cd8bc76f002b46bde3460b712e8d2f159b20eb899ead2147722615ea805f6"
+        "timestamp": 1734345081238,
+        "id": "19c44707ea1cc53b699190bea179582b2e947bb59d9695da5961b9cc11e7dd93"
     }]
 }' | nc localhost 3001
+```
+
+# Build a transaction
+```bash
+cargo run --bin build-transaction -- \
+--sender 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20 \
+--recipient 201f1e1d1c1b1a191817161514131211100f0e0d0c0b0a090807060504030201 \
+--amount 100 \
+--private-key 0000000000000000000000000000000000000000000000000000000000000000
 ```
---
+++ src/build-transaction.rs
@@ -0,0 +1,75 @@
+use address::Address;
+use clap::Parser;
+use ed25519_dalek::Signer;
+use ed25519_dalek::SigningKey;
+use serde_json::json;
+
+mod address;
+mod transaction;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    private_key: String,
+
+    #[arg(long)]
+    amount: u64,
+
+    #[arg(long)]
+    sender: String,
+
+    #[arg(long)]
+    recipient: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    // Convert hex private key to bytes
+    let private_key_bytes = hex::decode(args.private_key).expect("Invalid private key hex");
+    let private_key_array: [u8; 32] = private_key_bytes
+        .try_into()
+        .expect("Private key must be 32 bytes");
+
+    let signing_key = SigningKey::from_bytes(&private_key_array);
+
+    // Convert hex addresses to bytes
+    let sender_bytes = hex::decode(args.sender).expect("Invalid sender address hex");
+    let sender_array: [u8; 32] = sender_bytes
+        .try_into()
+        .expect("Sender address must be 32 bytes");
+
+    let recipient_bytes = hex::decode(args.recipient).expect("Invalid recipient address hex");
+    let recipient_array: [u8; 32] = recipient_bytes
+        .try_into()
+        .expect("Recipient address must be 32 bytes");
+
+    let tx = transaction::RawTransaction::new(
+        Address::from(sender_array),
+        Address::from(recipient_array),
+        args.amount,
+    )
+    .unwrap();
+
+    let signature = signing_key.sign(&tx.id.0);
+
+    let json_output = json!({
+        "jsonrpc": "2.0",
+        "method": "submitTransaction",
+        "params": [{
+            "from": hex::encode(tx.from),
+            "to": hex::encode(tx.to),
+            "amount": tx.amount,
+            "public_key": hex::encode(signing_key.verifying_key().as_bytes()),
+            "signature": {
+                "R": hex::encode(signature.r_bytes()),
+                "s": hex::encode(signature.s_bytes())
+            },
+            "timestamp": tx.timestamp,
+            "id": hex::encode(tx.id.0)
+        }]
+    });
+
+    println!("{}", serde_json::to_string_pretty(&json_output).unwrap());
+}
